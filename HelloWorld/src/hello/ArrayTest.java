a2c1a42ed21cf943a8f8b178d52700bb1c994284
User <User@YD03-04>
2019-09-16 17:48:40 +0900

    for

---
+++ HelloWorld/src/hello/ArrayTest.java
@@ -0,0 +1,30 @@
+package hello;
+
+import java.util.Scanner;
+
+public class ArrayTest {
+	public static void main(String[] args) {
+		/*Scanner sc = new Scanner(System.in);
+		System.out.println("input int type");
+		int a = sc.nextInt();
+		System.out.println("next input");
+		int b = sc.nextInt();
+		System.out.println("next input");
+		int c = sc.nextInt();
+
+		int sum = a + b + c;
+		System.out.println("합: " + sum);
+
+	    int[] intAry = new int[5];
+		for(int i=0; i<intAry.length;i++) {
+	    System.out.println("정수출력해라");
+		intAry[i] = sc.nextInt();
+		}
+		for(int i=0; i< intAry.length;i++) { 
+			
+			System.out.println(intAry[i]);
+
+		}*/
+		
+	}		
+}
---
+++ HelloWorld/src/hello/Gugudan.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class Gugudan {
+	public static void main(String[] args) {
+		/*for(int i=1;i<=9;i++) {
+			for(int j=2;j<=9;j++) {
+				System.out.print(j + " * " + i + " = " + i*j +"		");
+			}
+			System.out.println();
+		}*/
+		int i=1;
+		while (i<=9) {
+			int j=2;
+			while (j<=9) {
+				System.out.print(j + " * " + i + " = " +  i*j + "		");
+			j++;
+			} break;
+		}
+		i++; 
+
+	}
+}
